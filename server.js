65c78f71674dee147f9a1e64275db086f559b59e
Tokam340 <56760657+Tokam340@users.noreply.github.com>
2025-11-13 17:16:04 +0100

    Update cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: '*',
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
